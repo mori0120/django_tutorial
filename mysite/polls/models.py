7781b6170dfb6ab721e78635003a0b5d0f581f6d
Mori Yasuaki <mori.yasuaki18@gmail.com>
2022-12-30 18:15:17 +0000

    tutorial part3

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
-        return str(self.question_text)
+        return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
@@ -16,4 +16,4 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
-        return str(self.choice_text)
+        return self.choice_text
